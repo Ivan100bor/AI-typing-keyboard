2a544f092633a878eab117e1d71b54a192d6d2c1
Ivan Borisov <154272204+Ivan100bor@users.noreply.github.com>
2023-12-18 21:27:00 +0200

    Create keys.go

---
+++ keys.go
@@ -0,0 +1,10 @@
+		DisableFlagsInUseLine: true,
+		ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
+		Args:                  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			switch args[0] {
+			case "bash":
+				err := cmd.Root().GenBashCompletion(os.Stdout)
+				if err != nil {
+					panic(err)
+				}
